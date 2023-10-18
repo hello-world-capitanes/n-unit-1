2236b131b72bec51950069051e2a23cf5d9c5f65
BABEL-SI\sergio.claver <sergio.fclaver@alumnos.upm.es>
2023-10-18 11:17:43 +0200

    Updated models

---
+++ Scoring.Core/Services/CalculatePreScoring/DenyRules.cs
@@ -0,0 +1,12 @@
+﻿using Scoring.Core.Interfaces.CalculatePreScoring;
+
+namespace Scoring.Core.Services.CalculatePreScoring
+{
+    internal class DenyRules : IDenyRules
+    {
+        public bool DenniedRules()
+        {
+            return true;
+        }
+    }
+}
---
+++ Scoring.Model/Entities/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Scoring.Model.Entities
+{
+    internal class Persona
+    {
+        public DateTime FechaNacimiento { get; set; }
+        public double IngresosNetosAunales { get; set; } //Autonomo y/o asalariado
+        public double IngresosBrutosAnuales { get; set; } //Solo autonomo
+        public DateTime FechaInicioEmpleo { get; set; }
+        public string Nacionalidad { get; set; }
+        public string CIF { get; set; }
+    }
+}
