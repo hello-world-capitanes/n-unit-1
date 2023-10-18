5731913f900b7a7c39ba13cbd0a194b34207ff39
BABEL-SI\oscar.mesa <oscar.mesa@babelgroup.com>
2023-10-18 14:01:44 +0200

    Validaciones Ingresos (80 y Neto)

---
+++ Scoring.Core.UnitTest/CalculatePreScoring/ApproveRules/ValidarInversionMenorQueLimiteTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using Scoring.Core.Services.CalculatePreScoring.ApproveRules;
+using Scoring.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoring.Core.UnitTest.CalculatePreScoring.ApproveRules
+{
+    internal class ValidarInversionMenorQueLimiteTest
+    {
+        private ValidarInversionMenorQueLimite _validarInversionMenorQueLimite;
+        [SetUp]
+        public void Setup()
+        {
+            _validarInversionMenorQueLimite = new ValidarInversionMenorQueLimite();
+        }
+
+        [Test]
+        public void Should_True_When_Inversion_Menor_Limite()
+        {
+            //Given
+            Solicitud solicitud = new Solicitud();
+
+
+            solicitud.Inversion = 99;
+            double limite = 80000;
+
+            //When
+            bool resultado = _validarInversionMenorQueLimite.IsApproved(solicitud, limite);
+
+            //Then
+            Assert.IsTrue(resultado);
+        }
+
+        [Test]
+        public void Should_True_When_Inversion_Igual_Limite()
+        {
+            //Given
+            Solicitud solicitud = new Solicitud();
+
+            solicitud.Inversion = 80000;
+            double limite = 80000;
+
+            //When
+            bool resultado = _validarInversionMenorQueLimite.IsApproved(solicitud, limite);
+
+            //Then
+            Assert.IsTrue(resultado);
+        }
+
+        [Test]
+        public void Should_True_When_Inversion_Mayor_IngresosNeto()
+        {
+            //Given
+            Solicitud solicitud = new Solicitud();
+
+            solicitud.Inversion = 80001;
+            double limite = 80000;
+
+            //When
+            bool resultado = _validarInversionMenorQueLimite.IsApproved(solicitud, limite);
+
+            //Then
+            Assert.IsFalse(resultado);
+        }
+    }
+}
--- Scoring.Model/Entities/Persona.cs
+++ Scoring.Model/Entities/Persona.cs
@@ -5,7 +5,7 @@ namespace Scoring.Model.Entities
     internal class Persona
     {
         public DateTime FechaNacimiento { get; set; }
-        public double IngresosNetosAunales { get; set; } //Autonomo y/o asalariado
+        public double IngresosNetosAnuales { get; set; } //Autonomo y/o asalariado
         public double IngresosBrutosAnuales { get; set; } //Solo autonomo
         public DateTime FechaInicioEmpleo { get; set; }
         public string Nacionalidad { get; set; }
